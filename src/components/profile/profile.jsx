dd97c2c9912e91da676666df854ec3f5c2b8550e
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-06-03 13:33:37 +0200

    fixes profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import defaultImg from '../../img/default-img.jpg';
 
-export default function createProfile({
+export default function profile({
   imgUrl = defaultImg,
   name,
   tag,
   location,
-  followers,
-  views,
-  likes,
+  stats,
 }) {
   return (
     <div className="profile">
@@ -22,27 +20,29 @@ export default function createProfile({
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
           <span className="label">Views</span>
-          <span className="quantity">{views}</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
 
-createProfile.propTypes = {
+profile.propTypes = {
   imgUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
