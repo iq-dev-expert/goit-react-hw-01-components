970917e9bd99d831b16a6d5c2d8ea061e61d9521
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-06-03 13:12:11 +0200

    add transaction history

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import ProfileItem from 'components/profile/profileItem';
 import Statistics from './components/statistics/statistics';
 import FriendList from './components/friendList/friendList';
+import Transactions from './components/transaction-history/transactionHistory';
 import Section from 'components/section';
 import userData from './data/user.json';
 import dataStatistics from './data/data.json';
 import dataFriends from './data/friends.json';
+import dataTransactions from './data/transactions.json';
 
 export default function App() {
   return (
@@ -15,6 +17,9 @@ export default function App() {
       <Statistics title="Upload stats" stats={dataStatistics} />
       <Section className="section-friend-list">
         <FriendList friends={dataFriends} />
+      </Section>
+      <Section className="section-transaction-history">
+        <Transactions items={dataTransactions} />
       </Section>
     </div>
   );
